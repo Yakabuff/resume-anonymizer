0ce94f65f851ab56426bd9658f1e0b3257caa4d9
yakabuff <yekeb00f@gmail.com>
2020-10-14 02:20:50 -0400

    added command line functionality

--- command_line.py
+++ command_line.py
@@ -2,12 +2,13 @@
 
 import argparse
 
+from anonymizer import resume_anonymizer
 
-# import resume_anonymizer
+
 class resumeAnonymizerCLI(object):
     def __init__(self):
-        # self.output = None
-        # self.input = None
+        self.output = None
+        self.input = None
         # self.fields = None
 
         self.parser = argparse.ArgumentParser()
@@ -23,50 +24,44 @@ class resumeAnonymizerCLI(object):
             '--manual',
             nargs = "+",
             default = [],
-            # action='store',
-
             help = "manually anonymize fields"
         )
         self.parser.add_argument(
-            '-o',
-            '--output',
-
-            action='store_true',
-            help = "export file path"
+            'input',
+            help = "input file path"
         )
 
         self.parser.add_argument(
-            '-i',
-            '--input',
-
-            action='store_true',
-            help = "select input path"
+            'output',
+            help = "output file path"
         )
 
     def extract_resume_data(self):
         args = self.parser.parse_args()
         if args.output:
-            print("output")
-            output = args.output
+            # print("output")
+            # print(args.output)
+            self.output = args.output
         if args.input:
-            print("input")
-            input = args.input
+            # print("input")
+            self.input = args.input
+            # print(input)
         if (args.manual and args.auto):
             self.parser.error('Cant have both')
         if args.manual:
             # print("List of items: {}".format(args.alist))
+            # import resume_anonymizer
             secret_fields = args.manual
+            resume_anonymizer.anonymize(self.input, self.output, secret_fields)
             print(secret_fields)
 
         if args.auto:
-            print("auto")
-
-
-
-
+            print("auto not complete yet")
+        if not(args.manual and args.auto):
+            self.parser.error('Must have either manual or auto')
 
 
 if __name__ == '__main__':
-    print("hi")
+
     cli_obj = resumeAnonymizerCLI()
     cli_obj.extract_resume_data()
